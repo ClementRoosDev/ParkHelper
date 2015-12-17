19738a2643b7d49939d1aa6b154124b1ea8505d8
Tematim <thomas.bernard@ynov.com>
2015-12-17 15:07:19 +0100

    Modification de l'api, ajout des pages et de la liaison de l'edmx

---
+++ ParkHelper.Api/Controllers/AttractionsControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ParkHelper.Data;
+
+namespace ParkHelper.Api.Controllers
+{
+    class AttractionsControler : ApiController
+    {
+        private List<Attraction> _attractions
+        {
+            get
+            {
+                using (var db = new ParcHelperEntities())
+                {
+                    return db.Attractions.ToList();
+                }
+            }
+        }
+
+        public IHttpActionResult GetAttractions(int id)
+        {
+            var attraction = _attractions.FirstOrDefault((p) => p.id == id);
+            if (attraction == null)
+            {
+                return NotFound();
+            }
+            return Ok(attraction);
+        }
+    }
+}
