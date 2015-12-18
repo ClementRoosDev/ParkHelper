9910244915ad60984a4309c6d280a3b9fb757ad4
Fab <fabien.sens@free.fr>
2015-12-18 11:37:03 +0100

    Commencement de l'implémentation du Pattern Repository

---
+++ ParcHelper.Api/Models/IParkHelperRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkHelper.Data;
+
+namespace ParkHelper.Api.Models
+{
+    public interface IParkHelperRepository
+    {
+        List<Attraction> GetAllAttractions();
+        bool SaveAll();
+    }
+}
