c5366e351dfe8126c1d4bc27ca87c4f22e7fc0d0
Tematim <thomas.bernard@ynov.com>
2015-12-18 11:47:23 +0100

    Debut test API et profil envoi sur azure

--- ParkHelperPortableDataTest/Program.cs
+++ ParkHelperPortableDataTest/Program.cs
@@ -8,8 +8,13 @@ namespace ParkHelperPortableDataTest
 {
     class Program
     {
+        private const string Urlattractions = "http://parkhelperapi.azurewebsites.net/api/ptattractions";
+
+        private const string Urlparours = "http://parkhelperapi.azurewebsites.net/api/ptattractions";
+
         static void Main(string[] args)
         {
+            
         }
     }
 }
