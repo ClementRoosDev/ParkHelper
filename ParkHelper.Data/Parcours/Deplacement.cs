4a4b782d98bdee92aefc871271069e416dca2646
ClementRoosDev <clement.roos@ynov.com>
2016-01-15 10:10:07 +0100

    Fix deplacements in API

--- ParkHelper.Data/Parcours/Deplacement.cs
+++ ParkHelper.Data/Parcours/Deplacement.cs
@@ -4,7 +4,6 @@ using System.Runtime.Serialization;
 // ReSharper disable once CheckNamespace
 namespace ParkHelper.Data
 {
-    [DataContract]
     [KnownType(typeof(Deplacement))]
     public class Deplacement : IElementDeParcours
     {
